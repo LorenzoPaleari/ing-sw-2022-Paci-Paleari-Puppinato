0518c9a71d3b9670d2c4f8626c7a59f79a0c28b5
emanuelePaci <emanuele.paci@mail.polimi.it>
2022-03-09 14:30:38 +0100

    add packages

---
+++ src/main/java/it/polimi/ingsw/cards/Card.java
@@ -0,0 +1,13 @@
+package it.polimi.ingsw.cards;
+
+public abstract class Card {
+    protected String name;
+
+    public Card(){
+        super();
+    }
+
+    public String getName() {
+        return name;
+    }
+}
