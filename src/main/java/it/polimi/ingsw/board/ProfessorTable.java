06865a2b300180b5f510c271b4b3d09137c36c95
emanuelePaci <emanuele.paci@mail.polimi.it>
2022-03-15 12:51:28 +0100

    added board

---
+++ src/main/java/it/polimi/ingsw/board/ProfessorTable.java
@@ -0,0 +1,15 @@
+package it.polimi.ingsw.board;
+
+import java.util.List;
+
+public class ProfessorTable {
+    private List<Professor> professors;
+
+    public void removeProfessor(Professor p){
+
+    }
+
+    public void addProfessor(Professor p){
+
+    }
+}
---
+++ src/main/java/it/polimi/ingsw/board/TowerCourt.java
@@ -0,0 +1,14 @@
+package it.polimi.ingsw.board;
+
+import java.util.Set;
+
+public class TowerCourt {
+    private Set<Tower> tower;
+
+    public void removeTower(int numT){
+
+    }
+    public void addTower(int numT){
+
+    }
+}
