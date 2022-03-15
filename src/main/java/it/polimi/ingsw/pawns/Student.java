ef731537da7bba40ad46a1f8716cf80c834f2945
Lorenzo Paleari <100212108+LorenzoPaleari@users.noreply.github.com>
2022-03-15 12:33:47 +0100

    Added Pawn classes

---
+++ src/main/java/it/polimi/ingsw/pawns/Student.java
@@ -0,0 +1,20 @@
+package it.polimi.ingsw.pawns;
+
+import it.polimi.ingsw.enumerations.PawnColor;
+import java.util.*;
+
+public class Student {
+    private PawnColor color;
+
+    public Student find(PawnColor c) {
+        return null;
+    }
+
+    public List<Integer> findAll(Set<Student> s) {
+        return null;
+    }
+
+    public int find(Set<Student> s, PawnColor c){
+        return 0;
+    }
+}
---
+++ src/main/java/it/polimi/ingsw/pawns/Tower.java
@@ -0,0 +1,7 @@
+package it.polimi.ingsw.pawns;
+
+import it.polimi.ingsw.enumerations.PawnColor;
+
+public class Tower {
+    private PawnColor color;
+}
