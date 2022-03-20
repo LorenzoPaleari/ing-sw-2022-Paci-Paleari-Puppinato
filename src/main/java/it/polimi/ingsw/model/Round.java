da429fd766dc6c48cd5da251f50f3616f413dfe0
Lorenzo Paleari <100212108+LorenzoPaleari@users.noreply.github.com>
2022-03-20 22:25:19 +0100

    Round class implemented

--- src/main/java/it/polimi/ingsw/model/Round.java
+++ src/main/java/it/polimi/ingsw/model/Round.java
@@ -1,19 +1,82 @@
 package it.polimi.ingsw.model;
 
+import it.polimi.ingsw.model.enumerations.PlayerState;
 import it.polimi.ingsw.model.player.Player;
 import java.util.*;
 
 public class Round {
     private List<Player> playerSequence;
+    private List<Player> nextSequence;
     private Turn turn;
+    private Boolean lastRound;
 
-    public List<Player> planningPhase(){
-        return null;
+    public Round(Turn turn, List<Player> player) {
+        this.turn = turn;
+        playerSequence = new LinkedList<Player>(player);
+        nextSequence = new LinkedList<Player>(player);
+    }
+
+    public boolean nextActionTurn(){
+        int index = playerSequence.indexOf(turn.getCurrentPlayer());
+        turn.getCurrentPlayer().changeState(PlayerState.WAIT);
+
+        if (index == playerSequence.size() - 1)
+            return false;
+        else{
+            Player p = playerSequence.get(index + 1);
+            turn.updatePlayer(p);
+            turn.resetRemainingMovements(playerSequence.size() + 1);
+            p.changeState(PlayerState.ACTION);
+            return true;
+        }
+    }
+
+    public void endPlanningPhase(){
+        int assistenteBasso = 11;
+        for (Player p : playerSequence){
+            if (p.getLastUsed().getWeight() < assistenteBasso){
+                nextSequence.set(0, p);
+            }
+        }
+
+        for (int i = 1; i <= 2; i++) {
+            if (i <= playerSequence.size()) {
+                nextSequence.set(i, getNextPlayer(nextSequence.get(i - 1)));
+            }
+        }
+
+        playerSequence = nextSequence;
+        playerSequence.get(0).changeState(PlayerState.ACTION);
+
+    }
+
+    public boolean nextPlanningTurn(){
+        int index = playerSequence.indexOf(turn.getCurrentPlayer());
+        turn.getCurrentPlayer().changeState(PlayerState.WAIT);
+
+        if (index == playerSequence.size() - 1)
+            return false;
+        else{
+            Player p = playerSequence.get(index + 1);
+            turn.updatePlayer(p);
+            p.changeState(PlayerState.PLANNING);
+            return true;
+        }
     }
 
     public void endRound(){
-
+            playerSequence.get(0).changeState(PlayerState.PLANNING);
     }
 
+    public void setLastRound(Boolean lastRound) {
+        this.lastRound = lastRound;
+    }
+
+    public Player getNextPlayer(Player p){
+        if (playerSequence.indexOf(p) == playerSequence.size())
+            return playerSequence.get(0);
+        else
+            return playerSequence.get(1 + playerSequence.indexOf(p));
+    }
 
 }
