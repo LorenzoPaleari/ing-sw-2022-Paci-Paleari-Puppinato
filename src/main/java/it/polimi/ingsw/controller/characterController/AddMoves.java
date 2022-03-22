2e3b13da95232e6a1d8f993a1bb8094d7ce6ad40
emanuelePaci <emanuele.paci@mail.polimi.it>
2022-03-22 15:45:20 +0100

    updated character decorator

--- src/main/java/it/polimi/ingsw/controller/characterController/AddMoves.java
+++ src/main/java/it/polimi/ingsw/controller/characterController/AddMoves.java
@@ -1,5 +1,6 @@
 package it.polimi.ingsw.controller.characterController;
 
+//paci
 public class AddMoves extends Decorator{
     @Override
     public void useCharacter() {
--- src/main/java/it/polimi/ingsw/controller/characterController/FakeMotherNature.java
+++ src/main/java/it/polimi/ingsw/controller/characterController/FakeMotherNature.java
@@ -1,5 +1,6 @@
 package it.polimi.ingsw.controller.characterController;
 
+//puppi
 public class FakeMotherNature extends Decorator{
     @Override
     public void useCharacter() {
