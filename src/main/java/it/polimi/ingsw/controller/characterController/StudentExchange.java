2ea0d16a94449fc494d5db36b2ee7961ef30cfa7
emanuelePaci <emanuele.paci@mail.polimi.it>
2022-03-22 16:38:16 +0100

    updated addMoves decorator

--- src/main/java/it/polimi/ingsw/controller/characterController/StudentExchange.java
+++ src/main/java/it/polimi/ingsw/controller/characterController/StudentExchange.java
@@ -2,6 +2,8 @@ package it.polimi.ingsw.controller.characterController;
 
 
 //paci
+//you may exchange up to 2 students between your
+//entrance and your dining room
 public class StudentExchange extends Decorator{
     @Override
     public void useCharacter() {
