1474ceac946173238c129fffce95f67f9568bd0b
Thomas <thomas.puppinato@mail.polimi.it>
2022-03-26 14:52:31 +0100

    Updated Character

--- src/main/java/it/polimi/ingsw/model/character/Character.java
+++ src/main/java/it/polimi/ingsw/model/character/Character.java
@@ -1,6 +1,22 @@
 package it.polimi.ingsw.model.character;
 
+import it.polimi.ingsw.controller.Context;
+import it.polimi.ingsw.model.Game;
+import it.polimi.ingsw.model.enumerations.CharacterType;
+import it.polimi.ingsw.model.enumerations.PawnColor;
+import it.polimi.ingsw.model.player.Player;
+import it.polimi.ingsw.model.table.Island;
+
 public abstract class Character {
-    public abstract void useCharacter();
+    public abstract CharacterType getType();
+    public abstract int getCosto();
+    public abstract void returnNoEntryTiles();
+    public abstract void activateCharacter(Context context);
+    public abstract void activateCharacter(Island island, PawnColor color, Player player);
+    public abstract void activateCharacter(PawnColor color, Game game);
+    public abstract void activateCharacter(Player player, PawnColor[] color);
+    public abstract void activateCharacter(Island island);
+
+
 }
 
