f366fd3c62c38f3f762994c822f4929f7fadd381
Lorenzo Paleari <100212108+LorenzoPaleari@users.noreply.github.com>
2022-03-31 00:42:07 +0200

    I didn't want stupid errors while testing

---
+++ src/test/java/it/polimi/ingsw/tableTest/BagTest.java
@@ -0,0 +1,13 @@
+package it.polimi.ingsw.tableTest;
+
+import it.polimi.ingsw.model.pawns.Student;
+import org.junit.jupiter.api.Test;
+
+import java.util.LinkedList;
+
+public class BagTest {
+    @Test
+    public LinkedList<Student> initialSetup(int numPlayer){
+        return null;
+    }
+}
