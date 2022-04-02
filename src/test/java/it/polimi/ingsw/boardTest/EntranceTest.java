e80943ff5eea34364f90f064c8c071936f6087d2
emanuelePaci <emanuele.paci@mail.polimi.it>
2022-04-02 17:34:10 +0200

    added entrance Test

--- src/test/java/it/polimi/ingsw/boardTest/EntranceTest.java
+++ src/test/java/it/polimi/ingsw/boardTest/EntranceTest.java
@@ -1,35 +1,47 @@
 package it.polimi.ingsw.boardTest;
 
+import it.polimi.ingsw.model.board.DiningRoom;
+import it.polimi.ingsw.model.board.Entrance;
 import it.polimi.ingsw.model.enumerations.PawnColor;
 import it.polimi.ingsw.model.pawns.Student;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.LinkedList;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class EntranceTest{
-        @Test
-        LinkedList<Student> getStudent() {
-            return null;
+        LinkedList<Student> stud;
+        Entrance entrance;
+
+        @BeforeEach
+        public void setUp(){
+                stud = new LinkedList<>();
+                entrance = new Entrance(stud);
+
         }
 
         @Test
-        Student removeStudent() {
-            return null;
+        void removeStudent(PawnColor c) {
+                Student temp = new Student(4);
+                assertEquals(temp, entrance.removeStudent(PawnColor.BLUE));
         }
 
         @Test
         void addStudent(List<Student> s) {
-
+                assertEquals(s, entrance.getStudent());
         }
         @Test
         void addStudent(Student s) {
-
+                assertEquals(true, entrance.getStudent().contains(s));
         }
 
         @Test
-        Student find(PawnColor color) {
-            return null;
+        void find(PawnColor color) {
+                Student temp = new Student(4);
+                assertEquals(temp, entrance.find(PawnColor.BLUE));
         }
 
 
