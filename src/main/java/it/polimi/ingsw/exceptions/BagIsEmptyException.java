664469d3afbd5a3e247c226c0129ec7eb7a90b21
Lorenzo Paleari <100212108+LorenzoPaleari@users.noreply.github.com>
2022-04-03 12:42:39 +0200

    Added Error Message

--- src/main/java/it/polimi/ingsw/exceptions/BagIsEmptyException.java
+++ src/main/java/it/polimi/ingsw/exceptions/BagIsEmptyException.java
@@ -1,4 +1,8 @@
 package it.polimi.ingsw.exceptions;
 
 public class BagIsEmptyException extends Exception {
+    @Override
+    public String getMessage() {
+        return "Bag is Empty";
+    }
 }
