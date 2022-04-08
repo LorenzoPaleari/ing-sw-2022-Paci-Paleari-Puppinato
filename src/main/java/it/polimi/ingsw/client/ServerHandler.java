d5051814e5f84e806e22c4fc91bf8dcb6fe08782
Emanuele Paci <100219115+emanuelePaci@users.noreply.github.com>
2022-04-08 16:30:06 +0200

    updated server handler

--- src/main/java/it/polimi/ingsw/client/ServerHandler.java
+++ src/main/java/it/polimi/ingsw/client/ServerHandler.java
@@ -8,21 +8,41 @@ import java.net.Socket;
 
 public class ServerHandler {
     private Socket socket;
+    private boolean isConnected = false;
     public ServerHandler(){
 
     }
     public void initConnection(String serverIp){
         try{
-            Socket socket = new Socket(serverIp, Server.PORT);
+            socket = new Socket(serverIp, Server.PORT);
             System.out.println("socket = " +  socket);
-            BufferedReader in = new BufferedReader( new InputStreamReader( socket.getInputStream()));
-            PrintWriter out = new PrintWriter( new BufferedWriter( new OutputStreamWriter( socket.getOutputStream())),true);
-            String str = in.readLine();
-            socket.close();
+            isConnected = true;
+            listen();
+            endConnection();
         }
         catch(IOException e){
-
         }
 
+
+    }
+    public void listen(){
+        while (isConnected) {
+            try {
+                BufferedReader in = new BufferedReader( new InputStreamReader( socket.getInputStream()));
+                PrintWriter out = new PrintWriter( new BufferedWriter( new OutputStreamWriter( socket.getOutputStream())),true);
+                String str = in.readLine();
+
+            } catch (IOException e) {
+                isConnected = false;
+            }
+        }
+    }
+    public void endConnection() {
+        try {
+            socket.close();
+            isConnected = false;
+        } catch (IOException e) {
+
+        }
     }
 }
