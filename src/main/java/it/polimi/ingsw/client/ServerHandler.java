351453767f2b73d33728e11799f95f694f84964d
Emanuele Paci <100219115+emanuelePaci@users.noreply.github.com>
2022-04-08 16:05:55 +0200

    updated server Handler

--- src/main/java/it/polimi/ingsw/client/ServerHandler.java
+++ src/main/java/it/polimi/ingsw/client/ServerHandler.java
@@ -9,18 +9,20 @@ import java.net.Socket;
 public class ServerHandler {
     private Socket socket;
     public ServerHandler(){
+
     }
-    public void initConnection(String serverIp) throws IOException {
-        InetAddress addr = InetAddress.getByName(null);
-        System.out.println("addr = " + addr);
-        Socket socket = new Socket(addr, Server.PORT);
-        try {
+    public void initConnection(String serverIp){
+        try{
+            Socket socket = new Socket(serverIp, Server.PORT);
             System.out.println("socket = " +  socket);
             BufferedReader in = new BufferedReader( new InputStreamReader( socket.getInputStream()));
             PrintWriter out = new PrintWriter( new BufferedWriter( new OutputStreamWriter( socket.getOutputStream())),true);
             String str = in.readLine();
-        } finally {
             socket.close();
         }
+        catch(IOException e){
+
+        }
+
     }
 }
