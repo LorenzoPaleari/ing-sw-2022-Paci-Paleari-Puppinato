e16e59754f198460c1c2851d0a7f21dfe76eccd9
Emanuele Paci <100219115+emanuelePaci@users.noreply.github.com>
2022-04-08 09:13:39 +0200

    added test socket

--- src/main/java/it/polimi/ingsw/client/Client.java
+++ src/main/java/it/polimi/ingsw/client/Client.java
@@ -1,4 +1,29 @@
 package it.polimi.ingsw.client;
 
+import it.polimi.ingsw.server.Server;
+
+import java.io.*;
+import java.net.InetAddress;
+import java.net.Socket;
+
 public class Client {
+    public static void main(String[] args) throws IOException {
+        InetAddress addr = InetAddress.getByName(null);
+        System.out.println("addr = " + addr);
+        Socket socket = new Socket(addr, Server.PORT);
+        try {
+            System.out.println("socket = " +  socket);
+            BufferedReader in = new BufferedReader( new InputStreamReader( socket.getInputStream()));
+            PrintWriter out = new PrintWriter( new BufferedWriter( new OutputStreamWriter( socket.getOutputStream())),true);
+            for(int i = 0; i < 10; i ++) {
+                out.println("ciao " + i);
+                String str = in.readLine();
+                System.out.println(str);
+            }
+            out.println("END");
+        } finally {
+            System.out.println("closing...");
+            socket.close();
+        }
+    }
 }
--- src/main/java/it/polimi/ingsw/server/Server.java
+++ src/main/java/it/polimi/ingsw/server/Server.java
@@ -1,16 +1,35 @@
 package it.polimi.ingsw.server;
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 
 
 public class Server {
+    public static final int PORT = 8080;
     public static void main(String[] args) throws IOException {
-                ServerSocket serverSocket = new ServerSocket(4999);
-                Socket connection = serverSocket.accept();
-
-                connection.close();
-                serverSocket.close();
+            ServerSocket s = new ServerSocket(PORT);
+            System.out.println("Started: " + s);
+            try {
+                Socket socket = s.accept();
+                try {
+                    System.out.println("Connection accepted: "+ socket);
+                    BufferedReader in = new BufferedReader(new InputStreamReader( socket.getInputStream()));
+                    PrintWriter out = new PrintWriter( new BufferedWriter( new OutputStreamWriter( socket.getOutputStream())),true);
+                    while (true) {
+                        String str = in.readLine();
+                        if (str.equals("END")) break;
+                        System.out.println("Echoing: " + str);
+                        out.println(str);
+                    }
+                }
+                finally {
+                    System.out.println("closing...");
+                    socket.close();
+                }
+            }
+            finally {
+                s.close();
             }
     }
+}
