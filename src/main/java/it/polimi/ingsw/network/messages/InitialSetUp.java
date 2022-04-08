f65d8166a8cefdfbb20aa3ae889eca62a0854217
Lorenzo Paleari <100212108+LorenzoPaleari@users.noreply.github.com>
2022-04-08 18:13:40 +0200

    Fix

--- src/main/java/it/polimi/ingsw/network/messages/InitialSetUp.java
+++ src/main/java/it/polimi/ingsw/network/messages/InitialSetUp.java
@@ -1,22 +1,18 @@
 package it.polimi.ingsw.network.messages;
 
 import it.polimi.ingsw.client.View;
-import it.polimi.ingsw.model.enumerations.TowerColor;
 import it.polimi.ingsw.network.ControllerViewMessage;
 import it.polimi.ingsw.network.MessageType;
 
 import java.io.Serializable;
-import java.util.List;
 
 public class InitialSetUp implements Serializable, ControllerViewMessage {
     private MessageType type;
     private boolean firstPlayer;
-    private List<TowerColor> colorsEvailable;
 
-    public InitialSetUp(boolean firstPlayer, List<TowerColor> colorsEvailable){
+    public InitialSetUp(boolean firstPlayer){
         type = MessageType.ControllerView;
         this.firstPlayer = firstPlayer;
-        this.colorsEvailable = colorsEvailable;
     }
 
     @Override
